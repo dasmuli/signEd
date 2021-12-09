5a82ba48e8a0a959a40eea682562b57b1dfaffe4
dasmuli <dasmuli@web.de>
2021-12-09 16:22:59 +0100

    ADD: mnemonics for public keys ^^

--- src/signEdMain.c
+++ src/signEdMain.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdint.h>
 #include "ed25519.h"
 #include "b64.h"
 
@@ -16,6 +17,32 @@ void phex(unsigned char* str, int len)
     printf("\n");
 }
 
+char* mnemonic1[] = { "the house", "the cat", "superman" };
+size_t mnemonic1_length = sizeof(mnemonic1)/sizeof(mnemonic1[0]);
+char* mnemonic2[] = { "eats", "crashes", "fries", "delivers" };
+size_t mnemonic2_length = sizeof(mnemonic2)/sizeof(mnemonic2[0]);
+char* mnemonic3[] = { "my neighbour", "a smelly pie", "a chandelier" };
+size_t mnemonic3_length = sizeof(mnemonic3)/sizeof(mnemonic3[0]);
+char* mnemonic4[] = { "in the face", "hidden from public", "and said hi" };
+size_t mnemonic4_length = sizeof(mnemonic4)/sizeof(mnemonic4[0]);
+
+void print_mnemonic_part( uint64_t** pvalue, 
+  int* length, char* mnemonic[], size_t mnemonic_length)
+{
+  int index = **pvalue % mnemonic_length;
+  printf("%s ", mnemonic[ index ] );
+  *pvalue += 8;
+  *length -= 8;
+}
+
+void print_mnemonic( unsigned char* data, int length )
+{
+  uint64_t* pvalue = (uint64_t*)data;
+  print_mnemonic_part( &pvalue, &length, mnemonic1, mnemonic1_length);
+  print_mnemonic_part( &pvalue, &length, mnemonic2, mnemonic2_length);
+  print_mnemonic_part( &pvalue, &length, mnemonic3, mnemonic3_length);
+  print_mnemonic_part( &pvalue, &length, mnemonic4, mnemonic4_length);
+}
 
 unsigned char seed[32];
 unsigned char public_key[32];
@@ -106,4 +133,6 @@ int main(int argc, char* argv[])
       exit( 3 );
     }
   }
+
+  print_mnemonic( public_key, 32 );
 }
