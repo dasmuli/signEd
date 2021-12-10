6369a45a45ef8813f98effdeb5a1eb600a91d5d3
dasmuli <dasmuli@web.de>
2021-12-10 17:39:27 +0100

    ADD: Command line options skeleton function from Erik O'Shaughnessy.

--- src/signEdMain.c
+++ src/signEdMain.c
@@ -6,9 +6,35 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdint.h>
+#include <libgen.h>
+#include <errno.h>
+#include <getopt.h>
 #include "ed25519.h"
 #include "b64.h"
 #include "key_data.h"
+
+#define OPTSTR "vi:o:f:h"
+#define USAGE_FMT  "%s [-v] [-f hexflag] [-i inputfile] [-o outputfile] [-h]"
+#define ERR_FOPEN_INPUT  "fopen(input, r)"
+#define ERR_FOPEN_OUTPUT "fopen(output, w)"
+#define ERR_DO_THE_NEEDFUL "do_the_needful blew up"
+#define DEFAULT_PROGNAME "george"
+
+extern int errno;
+extern char *optarg;
+extern int opterr, optind;
+
+typedef struct {
+  int           verbose;
+  uint32_t      flags;
+  FILE         *input;
+  FILE         *output;
+} options_t;
+
+int dumb_global_variable = -11;
+
+void usage(char *progname, int opt);
+int  do_the_needful(options_t *options);
 
 void phex(unsigned char* str, int len)
 {
@@ -52,4 +78,76 @@ int main(int argc, char* argv[])
   init_data();
   printf("mnemonic for public key:\n");
   print_mnemonic( public_key, 32 );
+
+  int opt;
+    options_t options = { 0, 0x0, stdin, stdout };
+
+    opterr = 0;
+
+    while ((opt = getopt(argc, argv, OPTSTR)) != EOF)
+       switch(opt) {
+           case 'i':
+              if (!(options.input = fopen(optarg, "r")) ){
+                 perror(ERR_FOPEN_INPUT);
+                 exit(EXIT_FAILURE);
+                 /* NOTREACHED */
+              }
+              break;
+
+           case 'o':
+              if (!(options.output = fopen(optarg, "w")) ){
+                 perror(ERR_FOPEN_OUTPUT);
+                 exit(EXIT_FAILURE);
+                 /* NOTREACHED */
+              }
+              break;
+
+           case 'f':
+              options.flags = (uint32_t )strtoul(optarg, NULL, 16);
+              break;
+
+           case 'v':
+              options.verbose += 1;
+              break;
+
+           case 'h':
+           default:
+              usage(basename(argv[0]), opt);
+              /* NOTREACHED */
+              break;
+       }
+
+    if (do_the_needful(&options) != EXIT_SUCCESS) {
+       perror(ERR_DO_THE_NEEDFUL);
+       exit(EXIT_FAILURE);
+       /* NOTREACHED */
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+void usage(char *progname, int opt) 
+{
+   fprintf(stderr, USAGE_FMT, progname?progname:DEFAULT_PROGNAME);
+   exit(EXIT_FAILURE);
+   /* NOTREACHED */
+}
+
+int do_the_needful(options_t *options) 
+{
+
+   if (!options) {
+     errno = EINVAL;
+     return EXIT_FAILURE;
+   }
+
+   if (!options->input || !options->output) {
+     errno = ENOENT;
+     return EXIT_FAILURE;
+   }
+
+   /* XXX do needful stuff */
+
+   return EXIT_SUCCESS;
 }
